032a5011eb91e323a6344ce6143271fea6d0472f
nerdswithcharisma <brian@nerdswithcharisma.com>
2014-05-11 19:17:33 -0500

    Update Blog Layout

--- blog/wp-content/themes/nerdy/single.php
+++ blog/wp-content/themes/nerdy/single.php
@@ -83,17 +83,17 @@
 <?php endwhile; ?>								
 <?php else : ?>
 					
-							<article id="post-not-found" class="hentry clearfix">
-					    		<header class="article-header">
-					    			<h1><?php _e("Oops, Post Not Found!", "bonestheme"); ?></h1>
-					    		</header>
-					    		<section class="entry-content">
-					    			<p><?php _e("Uh Oh. Something is missing. Try double checking things.", "bonestheme"); ?></p>
-					    		</section>
-					    		<footer class="article-footer">
-					    		    <p><?php _e("This is the error message in the single.php template.", "bonestheme"); ?></p>
-					    		</footer>
-							</article>
+    <article id="post-not-found" class="hentry clearfix">
+        <header class="article-header">
+            <h1><?php _e("Oops, Post Not Found!", "bonestheme"); ?></h1>
+        </header>
+        <section class="entry-content">
+            <p><?php _e("Uh Oh. Something is missing. Try double checking things.", "bonestheme"); ?></p>
+        </section>
+        <footer class="article-footer">
+            <p><?php _e("This is the error message in the single.php template.", "bonestheme"); ?></p>
+        </footer>
+    </article>
 					
 <?php endif; ?>
 
