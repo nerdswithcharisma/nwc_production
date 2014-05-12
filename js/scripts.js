d4585857662a0e654b4b40253457c11fe56027ea
nerdswithcharisma <brian@nerdswithcharisma.com>
2014-05-11 19:58:17 -0500

    Add js back in

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function(){
+    
+    /* nav toggle on home page */
+    jQuery('#nav-toggle').on('click', function(){
+        jQuery('#pushContent').toggleClass('content--open');  
+        jQuery('#pushNav').toggleClass('navigation--open');
+    });
+});
